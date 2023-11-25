9934947621fafe827927816dfb4dd1900cd15278
Noman Ali <noman.ali.abbasy@gmail.com>
2023-11-25 20:56:53 +0300

    Added session bean with commandButton

--- getting-started-with-jsf/src/main/java/edu/learn/hello/SessionBeanDemo.java
+++ getting-started-with-jsf/src/main/java/edu/learn/hello/SessionBeanDemo.java
@@ -1,4 +1,34 @@
 package edu.learn.hello;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "sessionBeanDemo")
+@SessionScoped
 public class SessionBeanDemo {
+    private String view;
+
+    @PostConstruct
+    public void init() {
+        this.view = "Initial value";
+    }
+
+    public void changeView() {
+        System.out.println("View value: %s".formatted(this.view));
+    }
+
+    public String changeView(String view) {
+        System.out.println("Old value: %s, New value: %s".formatted(this.view, view));
+        this.view = view;
+        return this.view;
+    }
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
 }
